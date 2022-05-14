bd8db6a9f199022ef4599e8edc439d03e0d567f8
Mistred <spiridonov24@gmail.com>
2022-05-14 23:26:41 +0300

    Добавьте файлы проекта.

---
+++ GraphOfFunctions/View/FuncGraph.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GraphOfFunctions.View
+{
+    /// <summary>
+    /// Логика взаимодействия для FuncGraph.xaml
+    /// </summary>
+    public partial class FuncGraph : UserControl
+    {
+        public FuncGraph()
+        {
+            InitializeComponent();
+            chart.ChartAreas.Add(new ChartArea("Default"));
+            chart.ChartAreas[0].AxisX.MajorGrid.Enabled = false;
+            System.Windows.Forms.DataVisualization.Charting.Series series = new System.Windows.Forms.DataVisualization.Charting.Series("1");
+            series.ChartArea = "Default";
+            chart.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
+            chart.Series.Add(series);
+            chart.ChartAreas[0].AxisX.ArrowStyle =
+        System.Windows.Forms.DataVisualization.Charting.AxisArrowStyle.Lines;
+            chart.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            chart.Series[0].Color= System.Drawing.Color.Green;
+           
+        }
+    }
+}
---
+++ GraphOfFunctions/ViewModel/MainViewModel.cs
@@ -0,0 +1,51 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace GraphOfFunctions.ViewModel
+{
+   public class MainViewModel:ReactiveObject
+    {
+        private View.FuncGraph FuncGraph { get; set; }   
+
+        public UserControl MainControl { get; set; }    
+
+        public MainViewModel()
+        {
+            FuncGraph=new View.FuncGraph();
+            FuncGraph.DataContext=this;
+            MainControl=FuncGraph;
+            CreacteGraphTest();
+        }
+      private void CreacteGraphTest()
+        {
+            double x = -10;
+            double x2 = 10;
+            for(double i =x;i<=x2;i++)
+            {
+                double y= sqrt(Math.Pow(i, 2)* Math.Cos(i)+2);
+                Console.WriteLine(y);
+                FuncGraph.chart.Series[0].Points.AddXY(i, y);
+            }
+            //double y = Math.Sqrt(Math.Pow(x,2)* Math.Cos(x)+2);
+            
+        }
+        private double sqrt(double x)
+        {
+            if(x<0)
+            {
+                x=Math.Sqrt(-x);
+                x=0-x;
+            }
+            else
+            {
+                x=Math.Sqrt(x);
+            }
+            return x;
+        }
+    }
+}
